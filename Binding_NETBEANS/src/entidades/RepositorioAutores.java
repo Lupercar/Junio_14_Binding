2f3774be64046be762bd87a50a753de550b3f194
Lupercar <hotolete@gmail.com>
2016-09-17 22:55:41 +0200

    Creo dos libros y se los añado a Autor1

--- Binding_NETBEANS/src/entidades/RepositorioAutores.java
+++ Binding_NETBEANS/src/entidades/RepositorioAutores.java
@@ -7,6 +7,11 @@ public class RepositorioAutores {
     static private List<Autor> autores = new ArrayList<>(); 
     
     static{
-        autores.add(new Autor(1, "Autor1")); 
+        List<Libro> libros = new ArrayList<>();
+        
+        libros.add(new Libro(1, "Libro1")); 
+        libros.add(new Libro(2, "Libro2"));
+        
+        autores.add(new Autor(1, "Autor1", libros)); 
     }
 }//fin class entidades.RepositorioAutores
