95e943e90eff7a64d032191e1dad9cf96d48f11b
Lupercar <hotolete@gmail.com>
2016-09-17 22:25:09 +0200

    Creo Repositorio de Autor que tendrá una lista de autores.

---
+++ Binding_NETBEANS/src/entidades/RepositorioAutores.java
@@ -0,0 +1,8 @@
+package entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioAutores {
+    private List<Autor> autores = new ArrayList<>(); 
+}//fin class entidades.RepositorioAutores
