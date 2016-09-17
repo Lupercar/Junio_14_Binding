75b741e07c34f7a4999b31d8af45d7330329d655
Lupercar <hotolete@gmail.com>
2016-09-17 22:58:43 +0200

    Puedo crear una variable libros2 y repetir todo el proceso anterior

--- Binding_NETBEANS/src/entidades/RepositorioAutores.java
+++ Binding_NETBEANS/src/entidades/RepositorioAutores.java
@@ -13,5 +13,12 @@ public class RepositorioAutores {
         libros.add(new Libro(2, "Libro2"));
         
         autores.add(new Autor(1, "Autor1", libros)); 
+        
+        List<Libro> libros2 = new ArrayList<>();
+        
+        libros2.add(new Libro(3, "Libro3")); 
+        libros2.add(new Libro(4, "Libro4"));
+        
+        autores.add(new Autor(2, "Autor2", libros2)); 
     }
 }//fin class entidades.RepositorioAutores
