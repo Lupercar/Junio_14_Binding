7f5b0dd23c7465eac7223d3b2327c59bb482d972
Lupercar <hotolete@gmail.com>
2016-09-17 22:35:19 +0200

    Creo constructor static en RepositorioAutores, que se ejecutará antes que el de objeto y añado unos autores de ejemplo.

--- Binding_NETBEANS/src/entidades/RepositorioAutores.java
+++ Binding_NETBEANS/src/entidades/RepositorioAutores.java
@@ -5,4 +5,8 @@ import java.util.List;
 
 public class RepositorioAutores {
     static private List<Autor> autores = new ArrayList<>(); 
+    
+    static{
+        autores.add(new Autor(1, "Autor1")); 
+    }
 }//fin class entidades.RepositorioAutores
